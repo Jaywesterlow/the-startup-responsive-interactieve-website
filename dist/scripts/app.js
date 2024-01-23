789ec830db438710af4df5484a18cc0a97a1d6ba
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-01-23 22:57:48 +0100

    footer & fetch api

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -2,7 +2,6 @@ const menuBtn = document.getElementById("menu-btn");
 const menuContent = document.getElementById("menu-content");
 const body = document.querySelector('body');
 
-
 window.onload=function(){
   menuBtn.addEventListener("click", openMenu);
 }
@@ -12,3 +11,19 @@ function openMenu(){
   menuContent.classList.toggle('menuOpen')
   body.classList.toggle('maxHeight')
 }
+
+// app.js
+fetch('team.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        console.log(json);
+        // Hier kun je de JSON-gegevens verder verwerken
+    })
+    .catch(function() {
+        console.log("Er is een fout opgetreden bij het ophalen van het bestand.");
+    });
